29485a4972f2366f4801660cce575208ab1d2ca8
Matthew James <matthew.james@shift.com.au>
2026-07-27 20:57:05 +1000

    Make neovim transparent 🤤

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -3,6 +3,17 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
+    opts = {
+      transparent_background = true,
+      float = {
+        transparent = true,
+      },
+      dim_inactive = {
+        enabled = false,
+        shade = "dark",
+        percentage = 0.15,
+      },
+    },
   },
   {
     "LazyVim/LazyVim",
