95834c27f0c5f6d8202a8ab53bf5a8773e1e5647
Matthew James <matthew.james@shift.com.au>
2026-08-24 09:28:58 +1000

    Improve nvim bufferline config

---
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        always_show_bufferline = true,
+        enforce_regular_tabs = false,
+        max_name_length = 40,
+      },
+    },
+  },
+}
